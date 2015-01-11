7d08d221169a37befbc049afeacf717e4b4b7724
Henrique Bastos <henrique@bastos.net>
2015-01-11 14:46:05 -0200

    Add an runserver option to regenerate fixture images.

--- runserver.py
+++ runserver.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
+import sys
 from grid import app
-app.run(debug=True)
+
+
+def regenerate_fixture_images():
+    # Helper function to regenerate test fixture images
+    from grid.image import draw
+    d = draw(12, 95, 30, 15)
+    d.save('tests/thegrid.jpeg', format='JPEG', quality=100)
+    d.save('tests/thegrid.png', format='PNG')
+
+
+if __name__ == '__main__':
+
+    if 'fixtures' in sys.argv:
+        regenerate_fixture_images()
+        sys.exit()
+
+    app.run(debug=True)
