698be8753b17937e5738c65e0bbb1b031b1f16e4
Henrique Bastos <henrique@bastos.net>
2015-01-13 14:28:04 -0200

    Add test to style view

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -24,3 +24,11 @@ def test_image_content(app):
 
     with open('tests/thegrid.png', 'rb') as img:
         assert img.read() == rv.data
+
+
+def test_style(app):
+    rv = app.get('/12/95/30/15/')
+
+    assert 200 == rv.status_code
+    assert 'http://gridfor.me/i/12/95/30/15/' in rv.get_data(as_text=True)
+    assert 'text/css' == rv.mimetype
